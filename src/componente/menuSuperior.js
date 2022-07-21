4386f3141720d95f644825be40340a2bed677bcc
Pablo Zumarán <pablozumaran@inacapmail.cl>
2022-07-20 21:10:09 -0400

    actualizacion 20 de julio 21:10

--- src/componente/menuSuperior.js
+++ src/componente/menuSuperior.js
@@ -1,14 +1,18 @@
 import { Link, Outlet } from "react-router-dom"
-
+import './menuSuperior.css';
 const MenuSuperior = (props)=>{
     
    return(
-
+      
       <>
+      
       <Link to ="/pokemon"> pokemon</Link>
       <Link to ="/historial"> historial</Link>
       <Link to ="/combate"> combate</Link>
       <Link to ="/lista"> lista</Link>
+      <br/>
+      <input type="text" className = "buscador"/>
+      <button> BUSCAR </button>
       <br/>
       <Outlet/>
       </>
